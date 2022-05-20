d339738a76ef953ead4b6f773a7c38d4a2c686dc
Ramzan Kurbanov <ramkurbanov10@yandex.ru>
2022-05-20 18:34:01 +0300

    add html files and controller

--- src/main/java/com/example/crud_example/controller/UserController.java
+++ src/main/java/com/example/crud_example/controller/UserController.java
@@ -1,5 +1,25 @@
 package com.example.crud_example.controller;
 
+import com.example.crud_example.model.User;
+import com.example.crud_example.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+@Controller
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public String allUsers() {
+        List<User> users = userService.findAll();
+
+        return "index.html";
+    }
 }
