74c136f2b2b29a83d37c401b8bd90ec9da90a569
Ramzan Kurbanov <ramkurbanov10@yandex.ru>
2022-05-20 18:48:54 +0300

    add Model and post mapping

--- src/main/java/com/example/crud_example/controller/UserController.java
+++ src/main/java/com/example/crud_example/controller/UserController.java
@@ -3,7 +3,9 @@ package com.example.crud_example.controller;
 import com.example.crud_example.model.User;
 import com.example.crud_example.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -17,9 +19,23 @@ public class UserController {
     }
 
     @GetMapping("/users")
-    public String allUsers() {
+    public String allUsers(Model model) {
         List<User> users = userService.findAll();
-
-        return "index.html";
+        model.addAttribute("users", users);
+        return "user-list";
     }
+
+    @GetMapping("/user-create")
+    public String createUserForm(User user) {
+        return "user-create";
+    }
+
+    @PostMapping
+    public String addUser(User user) {
+        userService.save(user);
+        return "redirect:/users";
+    }
+
+
+
 }
